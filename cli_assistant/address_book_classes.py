cc917ba71edc71c8d9ae10552f51b1da2d386aa0
aitymori <anakholodko@gmail.com>
2023-01-27 23:12:26 +0200

    add class Birthday

--- cli_assistant/address_book_classes.py
+++ cli_assistant/address_book_classes.py
@@ -1,5 +1,8 @@
 from collections import UserDict
-from datetime import datetime
+from datetime import datetime, timedelta
+import pickle
+from pathlib import Path
+import re
 
 
 class Field:
@@ -36,6 +39,7 @@ class Email(Field):
     def __str__(self):
         return self._value.title()
 
+
 class Birthday(Field):
 
     @property
@@ -52,7 +56,7 @@ class Birthday(Field):
 
 
 class Record:
-    def __init__(self, name, phone, email=None, birthday=None):
+    def __init__(self, name, phone=None, email=None, birthday=None):
         self.name = name
         self.birthday = birthday
 
@@ -106,7 +110,6 @@ class Record:
                 self.emails.remove(email)
                 print("Email was deleted")
 
-
     """Робота з датою народження"""
     def add_birthday(self, birthday):
         birthday = Birthday(birthday)
@@ -120,8 +123,7 @@ class Record:
             print("Date of birth was deleted")
         else:
             print("such a date does not exist")
-        
-    
+
     def days_to_birthday(self):
         if self.birthday.value:
             try:
@@ -143,49 +145,19 @@ class Record:
             return f'Date of birth is not found. Please, add day of birth, if you want. '
 
 
-    def get_birthdays_per_week(self, range_of_days=7):
-
-        current_date = datetime.now().date()
-        for user in my_book:
-            user_name = user.get('name')
-            user_date = user.get('birthday') 
-            user_date = datetime.strptime(user_date, '%d/%m/%Y').date()
-            user_date = user_date.replace(year = current_date.year)
-            day_of_week = user_date.weekday()
-            delta_days = user_date - current_date
-
-            if 0 < delta_days.days <= range_of_days:
-                if day_of_week == 5 or day_of_week == 6:
-                    near_birthdays['name0'].append(user_name)
-                else:    
-                    near_birthdays[f"name{day_of_week}"].append(user_name)
-            elif 0 < delta_days.days > range_of_days:
-                continue
-            else:
-                user_date = user_date.replace(year=user_date.year + 1)
-                delta_days = user_date - current_date
-            if 0 < delta_days.days <= range_of_days:
-                if day_of_week == 5 or day_of_week == 6:
-                    near_birthdays['name0'].append(user_name)  
-                else:    
-                    near_birthdays[f"name{day_of_week}"].append(user_name)
-            elif 0 < delta_days.days > range_of_days:
-                continue
-        for i in range(5):
-            if near_birthdays[f"name{i}"] == []:
-                continue
-            else:
-                print(near_birthdays[f"day{i}"]+':', ", ".join(near_birthdays[f"name{i}"]))
-
     def contacts(self):
         phon = []
-        for i in self.phones:
-            phon.append(str(i))
-            result_phones = ", ".join(phon)
+        result_phones = ''
+        result_emails = ''
+        if len(self.phones) > 0:
+            for i in self.phones:
+                phon.append(str(i))
+                result_phones = ", ".join(phon)
         em = []
-        for i in self.emails:
-            em.append(str(i))
-            result_emails = ", ".join(em)
+        if len(self.emails) > 0:
+            for i in self.emails:
+                em.append(str(i))
+                result_emails = ", ".join(em)
         return f"name: {str(self.name.value)};\n" \
                f"phone: {result_phones};\n" \
                f"e-mail: {result_emails};\n" \
@@ -241,8 +213,6 @@ class AddressBook(UserDict):
             else:
                 print(near_birthdays[f"day{i}"]+':', ", ".join(near_birthdays[f"name{i}"]))
 
-    
-
 near_birthdays = {
                 'day0': 'Monday', 'name0': [],
                 'day1': 'Tuesday', 'name1': [],
@@ -251,8 +221,17 @@ near_birthdays = {
                 'day4': 'Friday', 'name4': []
                 }
 
+p = Path("address_book.bin")
+address_book = AddressBook()
+
+if p.exists():
+    with open("address_book.bin", "rb") as file:
+        address_book.data = pickle.load(file)
+
+
 
 if __name__ == "__main__":
+
     user_1 = Name("User_1")
     user_1_phone = Phone("034-1232-12312-12312")
     user_1_email = Email("user1@gmail.com")
@@ -269,7 +248,6 @@ if __name__ == "__main__":
 
     user_1_rec.add_birthday("31/01/2002")
 
-
     user_2 = Name("User_2")
     user_2_phone = Phone("097123123123")
     user_2_email = Email("user2@gmail.com")
@@ -277,13 +255,11 @@ if __name__ == "__main__":
     user_2_rec.add_phone("+380231231222")
     user_2_rec.add_phone("+38023132231222")
     user_2_rec.add_phone("+3802312323231222")
-    
+
     user_2_rec.add_birthday("26/03/2002")
     user_2_rec.delete_birthday("26/03/2002")
     user_2_rec.add_birthday("29/01/2002")
     print(user_2_rec.days_to_birthday())
-
-
 
     my_book = AddressBook()
     my_book.add_record(user_1_rec)
