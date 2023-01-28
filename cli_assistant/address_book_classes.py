858985c23550de3cc04750a1b132d7fa9d16ca56
OleksandrGnatiuk <oleksandr.gnatiuk@gmail.com>
2023-01-28 03:01:13 +0200

    changed handlers

--- cli_assistant/address_book_classes.py
+++ cli_assistant/address_book_classes.py
@@ -31,13 +31,28 @@ class Name(Field):
 class Phone(Field):
 
     def __str__(self):
-        return self._value.title()
+        return self._value
 
 
 class Email(Field):
 
     def __str__(self):
-        return self._value.title()
+        return self._value
+
+
+class Birthday(Field):
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        try:
+            if value != None:
+                self._value = datetime.strptime(value, '%d/%m/%Y').date()
+        except ValueError:
+            print (f'Please, input the date in format dd/mm/yyyy ')
 
 
 class Record:
@@ -95,6 +110,44 @@ class Record:
                 self.emails.remove(email)
                 print("Email was deleted")
 
+    """Робота з датою народження"""
+    def add_birthday(self, birthday):
+        """Adding date of birth"""
+        birthday = Birthday(birthday)
+        self.birthday = birthday
+        print("Date of birth was added")
+
+    def delete_birthday(self, old_birthday):
+        """Delete date of birth"""
+        old_birthday = Birthday(old_birthday)
+        if self.birthday.value == old_birthday.value:
+            self.birthday = None
+            print("Date of birth was deleted")
+        else:
+            print("such a date does not exist")
+
+    def days_to_birthday(self):
+        """How many days until user's birthday"""
+        if self.birthday.value:
+            try:
+                birthday_value = datetime.strptime(self.birthday.value, '%d/%m/%Y').date()
+                current_date = datetime.now().date()
+                user_date = birthday_value.replace(year = current_date.year)
+                self.delta_days = user_date - current_date
+                    
+                if 0 < self.delta_days.days:
+                    return f'Лишилось до Дня народження: {self.delta_days.days} днів.'
+                else:
+                    user_date = birthday_value.replace(year=user_date.year + 1)
+                    self.delta_days = user_date - current_date
+                    if 0 < self.delta_days.days:
+                        return f'Лишилось до Дня народження: {self.delta_days.days} днів.'
+            except ValueError:
+                return f'Please, input date in format dd/mm/yyyy '
+        else:
+            return f'Date of birth is not found. Please, add day of birth, if you want. '
+
+
     def contacts(self):
         phon = []
         result_phones = ''
@@ -111,7 +164,7 @@ class Record:
         return f"name: {str(self.name.value)};\n" \
                f"phone: {result_phones};\n" \
                f"e-mail: {result_emails};\n" \
-               f"birthday:{None};\n"
+               f"birthday:{self.birthday};\n"
 
 
 class AddressBook(UserDict):
@@ -129,6 +182,47 @@ class AddressBook(UserDict):
         self.data.pop(name)
         print("Record was deleted")
 
+    def get_birthdays_per_week(self, range_of_days=7):
+        """a list of users who have a birthday coming up soon"""
+        current_date = datetime.now().date()
+        for name, value in self.data.items():
+            user_name = name
+            user_date = value.birthday.value
+            user_date = datetime.strptime(user_date, '%d/%m/%Y').date()
+            user_date = user_date.replace(year = current_date.year)
+            day_of_week = user_date.weekday()
+            delta_days = user_date - current_date
+
+            if 0 < delta_days.days <= range_of_days:
+                if day_of_week == 5 or day_of_week == 6:
+                    near_birthdays['name0'].append(user_name)
+                else:    
+                    near_birthdays[f"name{day_of_week}"].append(user_name)
+            elif 0 < delta_days.days > range_of_days:
+                continue
+            else:
+                user_date = user_date.replace(year=user_date.year + 1)
+                delta_days = user_date - current_date
+                if 0 < delta_days.days <= range_of_days:
+                    if day_of_week == 5 or day_of_week == 6:
+                        near_birthdays['name0'].append(user_name)  
+                    else:    
+                        near_birthdays[f"name{day_of_week}"].append(user_name)
+                elif 0 < delta_days.days > range_of_days:
+                    continue
+        for i in range(5):
+            if near_birthdays[f"name{i}"] == []:
+                continue
+            else:
+                print(near_birthdays[f"day{i}"]+':', ", ".join(near_birthdays[f"name{i}"]))
+
+near_birthdays = {
+                'day0': 'Monday', 'name0': [],
+                'day1': 'Tuesday', 'name1': [],
+                'day2': 'Wednesday', 'name2': [],
+                'day3': 'Thurthday', 'name3': [],
+                'day4': 'Friday', 'name4': []
+                }
 
 p = Path("address_book.bin")
 address_book = AddressBook()
@@ -155,6 +249,8 @@ if __name__ == "__main__":
     user_1_rec.delete_email("arb@gmail.com")
     user_1_rec.change_phone("23123-1232-1132", "111111111111111")
 
+    user_1_rec.add_birthday("31/01/2002")
+
     user_2 = Name("User_2")
     user_2_phone = Phone("097123123123")
     user_2_email = Email("user2@gmail.com")
@@ -163,8 +259,15 @@ if __name__ == "__main__":
     user_2_rec.add_phone("+38023132231222")
     user_2_rec.add_phone("+3802312323231222")
 
+    user_2_rec.add_birthday("26/03/2002")
+    user_2_rec.delete_birthday("26/03/2002")
+    user_2_rec.add_birthday("29/01/2002")
+    print(user_2_rec.days_to_birthday())
+
     my_book = AddressBook()
     my_book.add_record(user_1_rec)
     my_book.add_record(user_2_rec)
+    my_book.get_birthdays_per_week()
     #my_book.del_record("User_1")
     print(my_book.show_book())
+    
