b6b469c63cf7204178856891b981869c852f3d95
OleksandrGnatiuk <oleksandr.gnatiuk@gmail.com>
2023-01-30 00:57:10 +0200

    created package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='assistant_team_project',
+      version='0.0.1',
+      description='CLI Assistant helps to manage the address book, notes, task book, organizes file in folder',
+      url='https://github.com/OleksandrGnatiuk/TeamProject_PyCore',
+      author='Oleksandr Gnatiuk', 
+      author_email='oleksandr.gnatiuk@gmail.com',
+      include_package_data=True,
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+            'markdown',
+            'prompt_toolkit',
+            'requests',
+            ],
+      entry_points={'console_scripts': ['assistant = cli_assistant.assistant:main']})
