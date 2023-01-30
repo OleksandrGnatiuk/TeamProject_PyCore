e87410d7eada1e99f4440957744ca29f805ef7c9
OleksandrGnatiuk <oleksandr.gnatiuk@gmail.com>
2023-01-30 14:13:33 +0200

    changed readme

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='assistant_team_project',
+setup(name='assistant_ota',
       version='0.0.1',
       description='CLI Assistant helps to manage the address book, notes, task book, organizes file in folder',
       url='https://github.com/OleksandrGnatiuk/TeamProject_PyCore',
-      author='Oleksandr Gnatiuk', 
-      author_email='oleksandr.gnatiuk@gmail.com',
+      author='Oleksandr Gnatiuk, Anastasiia Kholodko, Oleg Veisa, Artem Danilov, Tatiana Maximenko',
+      author_email='oleksandr.gnatiuk@gmail.com, anakholodko@gmail.com, oleg.veisa@gmail.com, ar.dantess@gmail.com, tetianamaximenko1994@gmail.com',
       include_package_data=True,
       license='MIT',
       packages=find_packages(),
