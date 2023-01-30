96a23494df39558ffba94eff6477cb4dd72dacdf
OleksandrGnatiuk <oleksandr.gnatiuk@gmail.com>
2023-01-30 15:01:17 +0200

    exception with currency

--- cli_assistant/currency.py
+++ cli_assistant/currency.py
@@ -4,9 +4,12 @@ import requests
 
 
 def get_currency(currencyname):
-    today = datetime.now().strftime("%Y%m%d")
-    URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=' + currencyname + '&date=' + today + '&json'
-    content = requests.get(URL)
-    headers = {'User-Agent':'Mozilla/5.0'}
-    result = json.loads(content.text)[0]
+    try:
+        today = datetime.now().strftime("%Y%m%d")
+        URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=' + currencyname + '&date=' + today + '&json'
+        content = requests.get(URL)
+        headers = {'User-Agent':'Mozilla/5.0'}
+        result = json.loads(content.text)[0]
+    except:
+        f"Problems with network"
     return f"\n{result['exchangedate']}  {result['txt']}: {result['rate']}\n"
